49eab79e68d0fe47ee71fe7c15cb174ef9209d1f
Merem <jesycaeyisi10@gmail.com>
2021-12-14 14:25:50 -0800

    first commit

---
+++ Eyisi's python assignment.py
@@ -0,0 +1,9 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/HP/AppData/Local/Programs/Python/Python310/Eyisi's python assignment.py
+Enter the value:5
+Enter the value:6
+Enter the value:7
+Enter the value:8
+6.928203230275509
